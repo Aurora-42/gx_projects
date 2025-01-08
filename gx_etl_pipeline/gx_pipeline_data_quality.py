8419ec0ea77d4863a7d9e514df07e7bd471f3023
aldantas <allandantas21@gmail.com>
2025-01-08 15:59:43 -0300

    fix import modules

--- gx_etl_pipeline/gx_pipeline_data_quality.py
+++ gx_etl_pipeline/gx_pipeline_data_quality.py
@@ -1,6 +1,5 @@
-import great_expectations as ge
+import great_expectations as gx
 from great_expectations.core.batch import BatchRequest
-from great_expectations.data_context import DataContext
 from great_expectations.validator.validator import Validator
 import pandas as pd
 
@@ -8,15 +7,18 @@ url = "https://github.com/hnawaz007/pythondataanalysis/blob/main/ETL%20Pipeline/
 df=pd.read_excel(url)
 print(df.head())
 
-context = DataContext()
+context = gx.get_context()
 
-data_source = context.data_source.add_pandas("pandas")
+data_source = context.data_sources.add_pandas("pandas")
 data_asset = data_source.add_dataframe_asset(name="pd dataframe asset")
 
 batch_definition = data_asset.add_batch_definition_whole_dataframe("batch definion")
 batch = batch_definition.get_batch(batch_parameters={"dataframe": df})
 
-expectation = ge.dataset.expect_column_values_to_be_in_set(
-    column="Product",
-    value_set=["A", "B", "C", "D", "E"],
+expectation = gx.expectations.ExpectColumnValuesToBeInSet(
+    column="ProductKey",
+    value_set=[0],
 )
+
+validation_result = batch.validate(expectation)
+print(f"resultado Expectation: {validation_result.success}")
