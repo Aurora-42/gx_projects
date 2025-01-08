88c1c97e29ca34759361cb549b7d7a15468942b2
aldantas <allandantas21@gmail.com>
2025-01-08 16:18:42 -0300

    change dataset & requirements
    change dataset to netflix imdb ratings, from hugging face

--- gx_etl_pipeline/gx_pipeline_data_quality.py
+++ gx_etl_pipeline/gx_pipeline_data_quality.py
@@ -3,9 +3,9 @@ from great_expectations.core.batch import BatchRequest
 from great_expectations.validator.validator import Validator
 import pandas as pd
 
-url = "https://github.com/hnawaz007/pythondataanalysis/blob/main/ETL%20Pipeline/Pytest/Session%20one/Product.xlsx?raw=true"
-df=pd.read_excel(url)
-print(df.head())
+url = "hf://datasets/mattrichmo/NetFlix-Imdb-Engagements-Films/cleanDataFilmFlat.csv"
+df = pd.read_csv(url)
+print(df)
 
 context = gx.get_context()
 
@@ -15,10 +15,9 @@ data_asset = data_source.add_dataframe_asset(name="pd dataframe asset")
 batch_definition = data_asset.add_batch_definition_whole_dataframe("batch definion")
 batch = batch_definition.get_batch(batch_parameters={"dataframe": df})
 
-expectation = gx.expectations.ExpectColumnValuesToBeInSet(
-    column="ProductKey",
-    value_set=[0],
+expectation = gx.expectations.ExpectColumnValuesToBeBetween(
+    column="imdbUserRating", min_value=0, max_value=10
 )
 
 validation_result = batch.validate(expectation)
-print(f"resultado Expectation: {validation_result.success}")
+print(f"resultado Expectation: {validation_result['success']}")
