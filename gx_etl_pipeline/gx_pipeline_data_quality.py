d9fbee1ab6464227c7d92157f8eda456500a7bd6
Allandantas21 <allandantas21.ad@gmail.com>
2025-01-10 13:37:26 -0300

    add readme in gx project

--- gx_etl_pipeline/gx_pipeline_data_quality.py
+++ gx_etl_pipeline/gx_pipeline_data_quality.py
@@ -12,16 +12,25 @@ context = gx.get_context()
 data_source = context.data_sources.add_pandas("pandas")
 data_asset = data_source.add_dataframe_asset(name="pd dataframe asset")
 
-batch_definition = data_asset.add_batch_definition_whole_dataframe("batch definion")
+batch_definition = data_asset.add_batch_definition_whole_dataframe("batch definition")
 batch = batch_definition.get_batch(batch_parameters={"dataframe": df})
 
-expectation = gx.expectations.ExpectColumnValuesToBeBetween(
-    column="imdbUserRating", min_value=0, max_value=10
-)
+# Define expectations
+expectations = [
+    gx.expectations.ExpectColumnValuesToBeBetween(
+        column="imdbUserRating", min_value=0, max_value=10
+    ),
+    gx.expectations.ExpectColumnValuesToNotBeNull(column="title"),
+    gx.expectations.ExpectColumnValuesToBeUnique(column="title"),
+    gx.expectations.ExpectColumnValuesToMatchRegex(
+        column="releaseYear", regex=r"^\d{4}$"
+    )
+]
 
-validation_result = batch.validate(expectation)
-
-if validation_result.success:
-    print("Succes in expectations!")
-else:
-    print("Failure in expectations!")
+# Validate expectations
+for expectation in expectations:
+    validation_result = batch.validate(expectation)
+    if validation_result.success:
+        print(f"Success in expectation: {expectation.expectation_type}")
+    else:
+        print(f"Failure in expectation: {expectation.expectation_type}")
